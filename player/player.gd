4c8d893503b6d3a14db01fa07b1fcd6cee4d7b24
Igor Almeida <igor.almeida@madeinweb.app>
2024-08-06 21:38:18 -0300

    feat: rooms

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed: int =55
 @onready var animation = $AnimationPlayer
+@onready var knife: Node2D = get_node("Knife")
 
 func handleInput():
 	var moveDirection = Input.get_vector( "ui_left", "ui_right","ui_up", "ui_down")
@@ -15,10 +16,12 @@ func updateAnimation():
 		if velocity.x < 0: direction = "Left"
 		elif velocity.x > 0: direction = "Right"
 		elif velocity.y < 0: direction = "Up"
-	
 		animation.play("walk" + direction)
 
 func  _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	
+func get_direction():
+	return global_position.direction_to(get_global_mouse_position())
--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -10,16 +10,3 @@ func _ready():
 func _process(delta):
 	if GameManager.camera_position != position:
 		position = GameManager.camera_position
-
-
-## USE THE MOUSE WHEEL TO ZOOM IN/ZOOM OUT, AND LEFT MOUSE BUTTON TO DRAG THE CAMERA
-
-func _input(event):
-	if event.get_action_strength("mouse_up"):
-		zoom = clamp(zoom + Vector2.ONE * 0.01 ,Vector2.ONE * 0.1,Vector2.ONE * 5.0)
-	
-	if event.get_action_strength("mouse_down"):
-		zoom = clamp(zoom - Vector2.ONE * 0.01 ,Vector2.ONE * 0.1,Vector2.ONE * 5.0)
-	
-	if event is InputEventMouseMotion and Input.is_action_pressed("MLB"):
-		global_position -= event.relative / zoom
